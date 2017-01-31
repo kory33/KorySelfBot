686ff04b9da5bdd633c5d0736848c9689bd04efe
Kory33 <konryoitdv@gmail.com>
2017-02-01 00:05:06 +0100

    remove redunduncy

--- commands/eval.js
+++ commands/eval.js
@@ -3,7 +3,7 @@ module.exports = {
         const evalString = args.join(" ");
         try{
             const result = eval(evalString);
-            return "*Evaluation result:* `" + eval(evalString) + "`";
+            return "*Evaluation result:* `" + result + "`";
         } catch (error) {
             return "*Error occured!*```CSS\n" + error.toString() + "```";
         }
