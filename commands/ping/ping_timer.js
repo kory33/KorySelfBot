a95ab25bb001aefbf12afa1c42c63e32af0dbd72
Kory33 <konryoitdv@gmail.com>
2017-02-01 23:10:20 +0100

    delete ping timer

--- commands/ping/ping_timer.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    reflect: message => {
-        const timeDiff = (new Date()) - message.createdAt;
-        message.edit(message.content + " *=> response time: `" + timeDiff + " ms`*");
-        return null;
-    }
-}
