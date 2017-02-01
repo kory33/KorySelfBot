004caba1f694bb38294a1f4b59a9169ab0f6ff1f
Kory33 <konryoitdv@gmail.com>
2017-02-01 17:45:48 +0100

    implement --json and --highlight options to eval

--- commands/eval.js
+++ commands/eval.js
@@ -2,6 +2,21 @@
 
 module.exports = {
     process: (args, event) => {
+        // json format output
+        const jsonOutput = (args[0] == "--json");
+        if(jsonOutput) {
+            args.shift();
+        }
+
+        // specification of highlight format
+        let highlightFormat = "";
+        if(jsonOutput) {
+            highlightFormat = "JSON";
+        } else if ((args[0] == "--highlight") || (args[0] == "-h")) {
+            args.shift();
+            highlightFormat = args.shift();
+        }
+
         // raw output
         const rawOutput = (args[0] == "--raw") || (args[0] == "-r");
         if(rawOutput) {
@@ -11,10 +26,18 @@ module.exports = {
         const evalString = args.join(" ");
 
         try{
-            const result = eval(evalString);
+            let result = eval(evalString);
 
             if(rawOutput) {
                 return result;
+            }
+
+            if(jsonOutput) {
+                result = JSON.stringify(result, null, 4);
+            }
+
+            if(highlightFormat !== "") {
+                result = highlightFormat + "\n" + result;
             }
 
             return ":desktop: *Evaluation result:*```" + result + "```";
