d64b061b993afe83d3b8d2b493e69d934fc1b760
Kory33 <konryoitdv@gmail.com>
2017-02-18 05:33:20 +0100

    implement setgame command

--- commands/setgame.js
+++ commands/setgame.js
@@ -4,12 +4,28 @@ const MessageCommand = require("./message_command.js");
 
 class SetGame extends MessageCommand {
     getResponse() {
-        // TODO
-        return "";
+        const commandArgs = this.args.slice();
+
+        const gamename = commandArgs.join(" ");
+
+        if (gamename === undefined) {
+            return ```setgame failed!```;
+        }
+
+        this.discordieClient.User.setGame(gamename);
+        
+        return `\`\`\`Set the game to ${gamename}\`\`\``;
     }
 
     run() {
-        // TODO
+        return super.run().then(sentMessage => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    sentMessage.delete();
+                    resolve();
+                }, 2000);
+            });
+        });
     }
 }
 
