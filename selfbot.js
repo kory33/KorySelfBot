0a2c3384a23edc4d5b5c9d8a94f3d61d6a053729
Kory33 <konryoitdv@gmail.com>
2017-03-23 21:14:51 +0100

    add varying reconnect interval

--- selfbot.js
+++ selfbot.js
@@ -7,25 +7,31 @@ const token = require("./.user_token.json");
 const settings = require("./settings.json");
 const client = new Discordie();
 
+let reconnectInterval = 1;
+
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
+    reconnectInterval = 1;
+
     console.log("Connected as: " + client.User.username);
 });
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-    if(client.User.id !== e.message.author.id) {
+    if (client.User.id !== e.message.author.id) {
         return;
     }
     (new SelfCommandProcessor(e, settings.prefix, client)).run();
 });
 
 client.Dispatcher.on(Discordie.Events.DISCONNECTED, e => {
-    console.log(e);
-    console.log("Disconnected from Discord... Trying to reconnect in 10 seconds");
+    const reconnectTime = reconnect_interval * 1000;
+    reconnectInterval *= 2;
+
+    console.log(`Disconnected from Discord... Trying to reconnect in ${reconnectTime} seconds`);
     return new Promise((resolve) => {
         setTimeout(() => {
             client.connect(settings);
             resolve();
-        }, 10000);
+        }, reconnectTime);
     });
 });
 
